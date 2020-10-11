481660495e0b868c7616430cea02aa128970d21b
Anwesh43 <anweshthecool0@gmail.com>
2020-10-11 19:10:00 +0530

    adding TrapezoidFillRect class

--- index.ts
+++ index.ts
@@ -190,3 +190,26 @@ class TFRNode {
         return this 
     }
 }
+
+class TrapezoidFillRect {
+
+    curr : TFRNode = new TFRNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
